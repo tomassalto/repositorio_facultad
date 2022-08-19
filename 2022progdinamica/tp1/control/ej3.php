f33266c622306bfb4029d3e0752ce65d30c9982e
tomassalto <tomasagustinsalto@gmail.com>
2022-08-19 19:39:29 -0300

    update

--- 2022progdinamica/tp1/control/ej3.php
+++ 2022progdinamica/tp1/control/ej3.php
@@ -8,12 +8,27 @@ if(isset($_POST)){
     }
     if(isset($_POST['edad'])){
         $edad = $_POST['edad'];
+        if($edad >= 18){
+            $mayorONo = "mayor";
+        }else{
+            $mayorONo = "menor";
+        }
     }
     if(isset($_POST['direccion'])){
         $direccion = $_POST['direccion'];
     }
+    if(isset($_POST['estudios'])){
+        $estudios = $_POST['estudios'];
+        if($_POST['estudios'] == "1- No tiene estudios"){
+            $stringEst = "No tiene estudios";
+        }else if($_POST['estudios'] == "2- Estudios primarios"){
+            $stringEst = "Estudios primarios";
+        }else{
+            $stringEst = "Estudios secundarios";
+        }        
+    }
 
-    echo "<p>Hola yo soy $nombre, $apellido tengo $edad años y vivo en $direccion</p>";
+    echo "<p>Hola yo soy $nombre, $apellido tengo $edad años, $mayorONo y vivo en $direccion, mis estudios son: $stringEst</p>";
 }
 
 
